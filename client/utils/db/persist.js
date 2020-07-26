37065f37f37352b2f882eb775f4af1a1d48c35cb
Ege Ozgirin <egeozin@gmail.com>
2020-07-26 10:53:31 -0400

    added sample persist functions

--- client/utils/db/persist.js
+++ client/utils/db/persist.js
@@ -2,8 +2,8 @@ require('dotenv').config({path:'./.env.local' })
 const firebase = require("firebase");
 // Required for side-effects
 require("firebase/firestore");
-
-console.log(process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID);
+const { questions, answers, users } = require('../fakeData');
+const { firestoreAutoId } = require('../utilFunctions');
 
 firebase.initializeApp({
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
@@ -12,9 +12,96 @@ firebase.initializeApp({
   });
   
 const db = firebase.firestore();
-
+/*
 db.collection("posts").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
         console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
     });
 });
+*/
+
+
+var usersRef = db.collection("users");
+
+const addUsersToDb = (u, userIDs) => {
+    return usersRef.add({
+        username: u.username,
+        name: u.name, 
+        description: "",
+        creationDate: firebase.firestore.FieldValue.serverTimestamp(),
+        lastAccessDate: firebase.firestore.FieldValue.serverTimestamp(),
+    }).then((userRef) => {
+        userIDs.push(userRef.id)
+    }).catch((err) => {
+        console.error("Error adding document: ", err);
+    })
+}
+
+async function usersForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
+    }
+}
+
+const addUsers = async () => {
+    var userIds = [];
+    await usersForEach(users, async (u)  => {
+        await addUsersToDb(u, userIds)
+    })
+    return userIds
+}
+
+/*
+const addPosts = async (userIds) => {
+    var postsRef = db.collection("postos");
+    questions.forEach((q) => {
+        var userid = userIds[Math.floor(Math.random() * userIds.length)];
+        postsRef.add({
+            title: q.title,
+            body: q.text,
+            tags: q.language,
+            postType: 1,
+            creationDate: firebase.firestore.FieldValue.serverTimestamp(),
+            answerCount: 3,
+            ownerUserId: userid,
+            ownerName: "", 
+            votes: Math.floor(Math.random() * 30)
+        })
+        .then((postRef) => {
+            console.log("Document written with ID: ", postRef.id);
+            var answer = answers[Math.floor(Math.random() * answers.length)];
+            answer.answers.forEach((a) => {
+                postsRef.add({
+                    title: "",
+                    body: a.text,
+                    postType: 2,
+                    creationDate: firebase.firestore.FieldValue.serverTimestamp(),
+                    answerCount: 3,
+                    ownerUserId: firestoreAutoId(),
+                    ownerName: a.name, 
+                    votes: Math.floor(Math.random() * 10)
+                })
+            
+            })
+        
+        })
+        .catch((err) => {
+            console.error("Error adding document: ", err);
+        });
+    })
+}*/
+
+const result = addUsers().then(userIds => {
+    console.log("users added!")
+    //addPosts(userIds)
+})
+
+
+
+
+/*
+postsRef.doc("LA").set({
+    name: "Los Angeles", state: "CA", country: "USA",
+    capital: false, population: 3900000,
+    regions: ["west_coast", "socal"] });
+*/
--- client/utils/utilFunctions.js
+++ client/utils/utilFunctions.js
@@ -1,3 +1,16 @@
-export const containsUrl = (text) => {
+exports.containsUrl = (text) => {
     return new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?").test(text)
 } 
+
+exports.firestoreAutoId = () => {
+    const CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  
+    let autoId = ''
+  
+    for (let i = 0; i < 20; i++) {
+      autoId += CHARS.charAt(
+        Math.floor(Math.random() * CHARS.length)
+      )
+    }
+    return autoId
+}

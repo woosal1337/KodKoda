b15f16778ca9e1a0a79e5a2be19ccffca54a2d06
egeozin <egeozin@mit.edu>
2020-08-08 16:21:04 -0400

    don't update upvote if user already upvoted it

--- client/pages/api/soru/upvote/index.js
+++ client/pages/api/soru/upvote/index.js
@@ -1,4 +1,5 @@
 import db from '../../../../utils/db/firebase_db'
+import Document from 'next/document';
 const firebase = require("firebase");
 // Required for side-effects
 require("firebase/firestore");
@@ -6,37 +7,45 @@ require("firebase/firestore");
 export default (req, res) => {
     const postInfo = JSON.parse(req.body)
     return new Promise((resolve, reject) => {
-      // TODO: check if user is upvoted then update posts
-      // e.g.: db.collection("Fruits").whereField("vitamins", arrayContains: "B6")
-      db
-        .collection('posts')
-        .doc(postInfo.postId)
-        .update({
-            voteCount: firebase.firestore.FieldValue.increment(1)
-        })
-        .then((doc) => {
-          const data = doc.data();
-          db
-            .collection('users')
-            .doc(postInfo.userId)
-            .update({
-              upvotes: firebase.firestore.FieldValue.arrayUnion(req.body.postId)
-            })
-            .then((doc) => {
-              res.json({status:"success"})
-              resolve()
-            })
-            .catch((error) => {
-              res.json({ error });
-              res.status(405).end();
-              resolve()
-            })
-        })
-        .catch((error) => {
+      var userRef = db.collection('users').doc(postInfo.userId)
+      userRef
+        .get().then((doc) => {
+          if (doc.data().upvotes.includes(postInfo.postId)) {
+            res.json({status:"success", docExists:true, error:null})
+            resolve()
+          } else {
+            userRef
+              .update({
+                upvotes: firebase.firestore.FieldValue.arrayUnion(postInfo.postId)
+              })
+              .then((doc) => {
+                db
+                .collection('posts')
+                .doc(postInfo.postId)
+                .update({
+                    voteCount: firebase.firestore.FieldValue.increment(1)
+                })
+                .then((doc) => {
+                  const data = doc.data();
+                  res.json({status:"success", docExists:false, error:null})
+                  resolve()
+                })
+                .catch((error) => {
+                  res.json({ error });
+                  res.status(405).end();
+                  resolve();
+                });
+              })
+              .catch((error) => {
+                res.json({ error });
+                res.status(405).end();
+                resolve()
+              })
+          }
+        }).catch((error) => {
           res.json({ error });
           res.status(405).end();
-          resolve();
-        });
-
+          resolve()
+        })
     })
 };

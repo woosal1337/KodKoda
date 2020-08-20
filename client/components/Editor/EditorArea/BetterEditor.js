5befe45853a2710dc2183302e9ce00de1140b28f
Sinan Işık <sinan.isik@ug.bilkent.edu.tr>
2020-08-20 14:57:30 +0300

    Add custom block for code blocks

--- client/components/Editor/EditorArea/BetterEditor.js
+++ client/components/Editor/EditorArea/BetterEditor.js
@@ -1,36 +1,65 @@
-import React, { useState, useEffect }  from 'react';
-import MUIRichTextEditor from 'mui-rte'
+import React, { useState, useEffect } from "react";
+import MUIRichTextEditor from "mui-rte";
 import { convertToRaw } from "draft-js";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        boxShadow: 'none',
-    },
-}))
+import CodeIcon from "@material-ui/icons/Code";
+import CodeBlock from "./CustomBlocks/CodeBlock";
 
-const BetterEditor = props => {
-    const classes = useStyles();
-    const [loading, setLoading] = useState(false);
-    const { forwardRef, label } = props;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    boxShadow: "none",
+  },
+}));
 
-    const onChange = editorState => {
-        props.handleChange('bodyText', convertToRaw(editorState.getCurrentContent()))
-    }
+const BetterEditor = (props) => {
+  const classes = useStyles();
+  const [loading, setLoading] = useState(false);
+  const { forwardRef, label } = props;
 
-    const onBlur = () => {
-        props.handleBlur('bodyText')
-    }
-    
-    if ( loading ) {
-        return (
-            <CircularProgress />
-        )
-    }
-
-    return (
-        <MUIRichTextEditor label={label} ref={forwardRef} onChange={onChange} onBlur={onBlur}  className={classes.root} controls={["title", "bold", "undo", "redo", "link", "bulletList", "quote", "code", "clear"]} />
+  const onChange = (editorState) => {
+    props.handleChange(
+      "bodyText",
+      convertToRaw(editorState.getCurrentContent())
     );
-}
+  };
+
+  const onBlur = () => {
+    props.handleBlur("bodyText");
+  };
+
+  if (loading) {
+    return <CircularProgress />;
+  }
+
+  return (
+    <MUIRichTextEditor
+      label={label}
+      ref={forwardRef}
+      onChange={onChange}
+      onBlur={onBlur}
+      className={classes.root}
+      controls={[
+        "title",
+        "bold",
+        "undo",
+        "redo",
+        "link",
+        "bulletList",
+        "quote",
+        "codeBlock",
+        "clear",
+      ]}
+      customControls={[
+        {
+          name: "codeBlock",
+          icon: <CodeIcon />,
+          type: "block",
+          blockWrapper: <CodeBlock />,
+        },
+      ]}
+    />
+  );
+};
 
 export default BetterEditor;

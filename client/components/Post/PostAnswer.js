0f107712fc729102e35bd83981825dffd7f6ec12
Cagri Zaman <iltempo@gmail.com>
2020-08-26 22:05:29 -0400

    Merge hell cleaned

--- client/components/Post/PostAnswer.js
+++ client/components/Post/PostAnswer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
@@ -15,20 +14,6 @@ import FormatQuoteIcon from "@material-ui/icons/FormatQuote";
 import Blockquote from "../Editor/EditorArea/CustomBlocks/Blockquote";
 import DeleteIcon from '@material-ui/icons/Delete';
 import DeletePopover from './DeletePopover'
-=======
-import React, { useState, useEffect } from 'react';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Divider from '@material-ui/core/Divider';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
-import ExpandLessIcon from '@material-ui/icons/ExpandLess';
-import DeletePopover from './DeletePopover'
-import Link from '../Link';
-import MUIRichTextEditor from 'mui-rte';
-import { MuiThemeProvider } from '@material-ui/core/styles'
->>>>>>> 47f98c2ead8a5de20589e31b3c939cb368bdff0a
 
 import { makeStyles } from "@material-ui/core/styles";
 import theme from "../../src/theme";
@@ -36,7 +21,6 @@ import palette from "../../src/palette";
 import EmojiBar from "./Emoji/EmojiBar";
 
 const updateTheme = {
-<<<<<<< HEAD
   ...theme,
   overrides: {
     ...theme.overrides,
@@ -55,96 +39,6 @@ const updateTheme = {
     },
   },
 };
-=======
-    ...theme,
-    overrides: {
-        ...theme.overrides,
-
-        MUIRichTextEditor: {
-            ...theme.overrides.MUIRichTextEditor,
-            editor: {
-                ...theme.overrides.MUIRichTextEditor.editor,
-                padding: 0,
-                minHeight: 100
-            },
-            editorContainer: {
-                ...theme.overrides.MUIRichTextEditor.editorContainer,
-                padding: 0
-            }
-        }
-    }
-}
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        boxShadow: 'none',
-    },
-    flexGrow: {
-        flexGrow: 1
-    },
-    title: {
-        fontSize: 36,
-        fontFamily: 'Hind, sans-serif',
-        fontWeight: 700,
-    },
-    rightTitle: {
-        lineHeight: '29px'
-    },
-    divider: {
-        marginBottom: 20,
-        backgroundColor: "#d7d7d7",
-    },
-    postContainer: {
-    },
-    postGridContainer: {
-        marginTop: 20
-    },
-    answersContainer: {
-    },
-    answerContainer: {
-        minHeight: 120,
-        //maxWidth:300
-    },
-    answerPoster: {},
-    answerPosterContainer: {
-        marginTop: 20
-    },
-    answerText: {
-        marginTop: 5,
-        fontSize: 16,
-        lineHeight: 1.5
-    },
-    buttons: {
-        marginTop: -6,
-        marginLeft: 4
-    },
-    voteButton: {
-        color: theme.palette.text.secondary,
-    },
-    voteCount: {
-    },
-    voteMore: {
-        fontSize: 40,
-        margin: -12
-    },
-    voteLess: {
-        fontSize: 40,
-        margin: -12,
-        marginTop: -18
-    },
-    languageButton: {
-        background: 'linear-gradient(45deg, var(--background-start) 30%, var(--background-end) 90%)',
-        borderRadius: 3,
-        boxShadow: 'none',
-        border: 0,
-        fontSize: 14,
-        fontWeight: 600,
-        color: 'white',
-        height: 24,
-        padding: '0 10px',
-    }
-}))
->>>>>>> 47f98c2ead8a5de20589e31b3c939cb368bdff0a
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -214,7 +108,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-<<<<<<< HEAD
 const PostAnswer = (props) => {
   const classes = useStyles();
   const [loading, setLoading] = useState(false);
@@ -297,62 +190,6 @@ const PostAnswer = (props) => {
                     {data.body.charAt(0).toUpperCase() + data.body.slice(1)}
                     </Typography>
                 )}
-=======
-const PostAnswer = props => {
-    const classes = useStyles();
-    const [loading, setLoading] = useState(false);
-    const { data, userId, userName, onMutate, handleDelete } = props
-    console.log(userId)
-    return (
-        <>
-            <Divider className={classes.divider} />
-            <Grid container direction="row" spacing={1} className={classes.answersContainer}>
-                <Grid item container direction="column" alignItems="left" xs={1} md={1} className={classes.buttons}>
-                    <Grid item >
-                        <IconButton edge="start" className={classes.voteButton} aria-label="menu">
-                            <ExpandLessIcon className={classes.voteMore} />
-                        </IconButton>
-                    </Grid>
-                    <Grid item >
-                        <Typography className={classes.voteCount}>{data.voteCount}</Typography>
-                    </Grid>
-                </Grid>
-                <Grid item xs={10} md={10} >
-                    <Grid container direction="column" justify={"space-between"} className={classes.answerContainer}>
-                        <Grid container direction="row" justify={"space-between"} className={classes.answerContainer}>
-
-                            <Grid item >
-                                {data.body.blocks ?
-                                    <MuiThemeProvider theme={updateTheme}>
-                                        <MUIRichTextEditor readOnly={true} toolbar={false} defaultValue={JSON.stringify(data.body)} />
-                                    </MuiThemeProvider>
-                                    :
-                                    <Typography variant="body1" component="body" className={classes.questionText}>
-                                        {data.body.charAt(0).toUpperCase() + data.body.slice(1)}
-                                    </Typography>
-                                }
-                            </Grid>
-                            {userId == data.ownerUserId ?
-                            <Grid item>
-                                <IconButton edge="start" className={classes.deleteButton} aria-label="delete">
-                                <DeletePopover data={data} userId={userId} userName={userName} handleDelete={handleDelete}/>
-                                </IconButton>
-                            </Grid>
-                            : <div />
-                        }
-                        </Grid>
-                        
-                        <Grid item >
-                            <Grid item className={classes.answerPosterContainer}>
-                                <Link href="/user/[id]/" as={`/user/${data.ownerUserId}`}>
-                                    <Typography className={classes.answerPoster}>
-                                        @{data.ownerName}
-                                    </Typography>
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Grid>
->>>>>>> 47f98c2ead8a5de20589e31b3c939cb368bdff0a
                 </Grid>
                 {userId == data.ownerUserId ?
                 <Grid item>
--- client/components/Post/PostAnswers.js
+++ client/components/Post/PostAnswers.js
@@ -15,7 +15,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-<<<<<<< HEAD
 const PostAnswers = (props) => {
   const classes = useStyles();
   const { data } = props;
@@ -48,29 +47,6 @@ const PostAnswers = (props) => {
               </Grid>
             );
           })}
-=======
-const PostAnswers = props => {
-    const classes = useStyles();
-    const { data, userId, userName, onMutate, handleDelete } = props
-    console.log(userId)
-    return (
-        <Grid container direction="row" spacing={1} className={classes.answersContainer}>
-            <Grid item xs={3} md={1} ></Grid>
-            <Grid item container xs={12} md={9} >
-                <Grid container direction="column" wrap="nowrap" spacing={3} className={classes.answersContainer}>
-                  {data.map((e, i) => {
-                      return (
-                          <Grid key={i} item>
-                            <PostAnswer data={e} userId={userId} userName={userName} onMutate={onMutate} handleDelete={handleDelete}/>
-                          </Grid>
-                          
-                        )
-                  })
-                  }
-                </Grid>
-            </Grid>
-            <Grid item xs md={2} ></Grid>
->>>>>>> Delete Api and UI components
         </Grid>
       </Grid>
       <Grid item xs md={2}></Grid>
